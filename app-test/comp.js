b63ae64ee46c9ae4fb724657e34b0f5f26382aae
akolag <akolag@gmail.com>
2018-10-28 15:25:52 -0400

    Create comp.js
    Sample of crypto code

---
+++ app-test/comp.js
@@ -0,0 +1,47 @@
+// simple cryptocurrency called comp in node.js.
+class Block{
+	constructor(index, data, prevHash){
+		this.index = index;
+                this.timestamp = Math.floor(Date.now() / 1000);
+		this.data = data;
+		this.prevHash = prevHash;
+	}
+}
+constructor(index, data, prevHash){
+	this.index = index;
+	this.timestamp = Math.floor(Date.now() / 1000);
+	this.data = data;
+	this.prevHash = prevHash;
+	this.hash = this.getHash();
+}
+
+getHash(){
+	return sha(JSON.stringify(this.data) + this.prevHash + this.index + this.timestamp);
+}
+class BlockChain{
+	constructor(){
+		this.chain = [];
+	}
+}
+addBlock(data){
+	let index = this.chain.length;
+	let prevHash = this.chain.length !== 0 ? this.chain[this.chain.length - 1].hash : 0;
+	let block = new Block(index, data, prevHash);
+
+	this.chain.push(block);
+}
+// Create another method in the same class and name it chainIsValid. 
+// In this function, we will be checking if the blockchain is following all the rules. 
+chainIsValid(){
+
+	for(var i=0;i<this.chain.length;i++){
+
+		if(this.chain[i].hash !== this.chain[i].getHash()) 
+			return false;
+
+		if(i > 0 && this.chain[i].prevHash !== this.chain[i-1].hash)
+			return false;
+	}
+
+	return true;
+}
